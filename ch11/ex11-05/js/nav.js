d23351c5acdce4a603980fc90a5dd212dedd2199
Lydia Hsiao-Mei Lin <Lydia.HMLin@gmail.com>
2023-05-05 15:41:30 +0800

    clean the folder

--- ch11/ex11-05/js/nav.js
+++
@@ -1,9 +0,0 @@
-//導覽列
-function navFunction() {
-	var topNav = document.getElementById('myTopnav');
-	if (topNav.className === 'nav') {
-	  topNav.className += ' responsive';
-	} else {
-	  topNav.className = 'nav';
-	}
-}
